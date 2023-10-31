34a38ad3dfb0dca12d5eb68e2af3a8f611ed88c2
TheJogMan <jogoyo620@gmail.com>
2023-10-31 17:57:37 -0400

    Initial Commit

---
+++ discordSyncSource/discordSync/DiscordSync.java
@@ -0,0 +1,12 @@
+package discordSync;
+
+import org.bukkit.plugin.java.*;
+
+public class DiscordSync extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		System.out.println("Hello World!");
+	}
+}
